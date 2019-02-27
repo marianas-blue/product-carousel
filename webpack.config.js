45af0f01b3ca4d07effe75771c8cb289567986b0
Greg Kittleson <greg.kittleson@gmail.com>
2019-02-26 16:47:29 -0800

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+// const HtmlPlugin = require('html-webpack-plugin');
+// const path = require('path');
+
+module.exports = {
+  entry: `${__dirname}/src/index.jsx`,
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+            ],
+          },
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader',
+      },
+      {
+        test: /\.css$/,
+        loader: 'css-loader',
+        query: {
+          modules: true,
+          localIdentName: '[name]__[local]___[hash:base64:5]',
+        },
+      },
+    ],
+  },
+};
