c0edfde96c9928dc315b9b2d4943022cc9692715
Greg Kittleson <greg.kittleson@gmail.com>
2019-03-07 15:22:36 -0800

    Refactor client to accept dynamic image URL

--- src/App/Carousel/Product/Product.jsx
+++ src/App/Carousel/Product/Product.jsx
@@ -3,34 +3,41 @@ import propTypes from 'prop-types';
 import product from './Product.css';
 import Rating from './Rating/Rating.jsx';
 
-const primeSprite = 'https://res.cloudinary.com/dcywbu46z/image/upload/h_20,f_auto/v1550951658/amazon-FEC/prime.png';
+const primeSprite =
+  'https://res.cloudinary.com/dcywbu46z/image/upload/h_20,f_auto/v1550951658/amazon-FEC/prime.png';
 
-const concatUrl = id => `https://res.cloudinary.com/dcywbu46z/image/upload/w_160,h_160,f_auto/v1550899094/amazon-FEC/item-${id}.jpg`;
+const concatUrl = id =>
+  `https://res.cloudinary.com/dcywbu46z/image/upload/w_160,h_160,f_auto/v1550899094/amazon-FEC/item-${id}.jpg`;
 
 const Product = ({
-  product: {
-    id, name, avgReview, price, isPrime, reviewCount,
-  },
+  product: { id, name, avgReview, price, isPrime, reviewCount, image }
 }) => (
   <div className={product.column}>
     <a href={`/products/${id}`}>
       <div className={product.ad_image}>
-        <div style={{ background: `url(${concatUrl(id)}`, backgroundSize: 'cover', height: '160px' }} alt="product was here" />
+        <div
+          style={{
+            background: `url(${image}`,
+            backgroundSize: 'cover',
+            height: '160px'
+          }}
+          alt="product was here"
+        />
       </div>
     </a>
-    <a href={`/products/${id}`} className={product.name}>{name}</a>
+    <a href={`/products/${id}`} className={product.name}>
+      {name}
+    </a>
     <div className={product.row}>
       <Rating avgReview={avgReview} />
-      <span className={product.review_count}>
-        {reviewCount}
-      </span>
+      <span className={product.review_count}>{reviewCount}</span>
     </div>
     <div className={product.row}>
       <span className={product.price}>{`$${price}`}</span>
       <span>
-        {isPrime === true
-          ? <img className={product.prime} src={primeSprite} alt="" />
-          : null }
+        {isPrime === true ? (
+          <img className={product.prime} src={primeSprite} alt="" />
+        ) : null}
       </span>
     </div>
   </div>
@@ -44,8 +51,8 @@ Product.propTypes = {
     price: propTypes.string.isRequired,
     isPrime: propTypes.bool.isRequired,
     reviewCount: propTypes.number.isRequired,
-    category: propTypes.string.isRequired,
-  }).isRequired,
+    category: propTypes.string.isRequired
+  }).isRequired
 };
 
 export default Product;
