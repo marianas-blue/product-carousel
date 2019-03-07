709027219f925faca889c9203310423fb2185647
Greg Kittleson <greg.kittleson@gmail.com>
2019-03-07 15:22:11 -0800

    Refactor client connection to remain open for app lifecycle

--- server/database/models.js
+++ server/database/models.js
@@ -2,28 +2,25 @@ const pg = require('pg');
 const dbString = require('../../config');
 
 const client = new pg.Client(dbString);
+client.connect();
 
 const selectRecs = async (id, name) => {
-  client.connect().catch(e => console.error(e.stack));
   return client
     .query(
       `SELECT products.* FROM products, recs WHERE recs.product_id = ${id} AND recs.rec_id = products.id`
     )
     .then(res => {
-      client.end();
       return res.rows;
     })
     .catch(e => console.error(e.stack));
 };
 
 const setRec = (id, oldId, newId) => {
-  client.connect().catch(e => console.error(e.stack));
   return client
     .query(
       `UPDATE recs SET rec_id = ${newId} WHERE product_id = ${id} AND rec_id = ${oldId};`
     )
     .then(res => {
-      client.end();
       return res.rows;
     })
     .catch(e => console.error(e.stack));
